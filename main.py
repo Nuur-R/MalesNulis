4e0e38095683a92a39f2d2b30ea21a72aa38f731
Firdaus Nuur Rhamadhan <93628058+Nuur-R@users.noreply.github.com>
2022-05-06 18:59:08 +0700

    Add files via upload

--- main.py
+++ main.py
@@ -1,18 +1,25 @@
 from flask import Flask,render_template, request
 from text_to_handwrite import text_to_handwriting
-
+import os
 
 app=Flask(__name__)
 
+# image
+picFolder = 'static/img'
+print(picFolder)
+app.config['UPLOAD_FOLDER'] = picFolder
+
 @app.route('/', methods=['GET', 'POST'])
 def home():
+    paragraph_picture = os.path.join(app.config['UPLOAD_FOLDER'], 'paragraph.png')
     # buat POST request
     if request.method=='POST':
         paragraph = request.form['paragraph']
         print(paragraph)
         text_to_handwriting(paragraph, "paragraph")
 
-    return render_template('home.html')
+    return render_template('home.html',pic=paragraph_picture)
 
 if __name__ == '__main__':
+    print("Server is running in localhost:5000")
     app.run(debug=True)
--- text_to_handwrite.py
+++ text_to_handwrite.py
@@ -3,4 +3,4 @@ from pywhatkit import text_to_handwriting as kit
 
 def text_to_handwriting(text, save_to):
     """Convert text to handwriting using pywhatkit"""
-    kit(text, save_to="results/"+save_to+".png")
+    kit(text, save_to="static/img/"+save_to+".png")
